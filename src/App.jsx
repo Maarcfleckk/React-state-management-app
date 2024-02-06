ee40a67dfd9056afb1d9fd7a2bd62f01eb0c8103
Fleki <marc.fleck@mataro.epiaedu.cat>
2024-02-06 20:18:39 +0100

    finish exercise 1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import { useState } from "react";
-
+import { Counter } from "./Counter.jsx";
 const App = () => {
   // save each button click in its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const handleIncrementGood = () => {
+    setGood((prevGood) => {
+      prevGood = prevGood + 1;
+      return prevGood;
+    });
+  };
+  const handleIncrementNeutral = () => {
+    setNeutral((prevNeutral) => {
+      prevNeutral = prevNeutral + 1;
+      return prevNeutral;
+    });
+  };
+  const handleIncrementBad = () => {
+    setBad((prevBad) => {
+      prevBad = prevBad + 1;
+      return prevBad;
+    });
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleIncrementGood}>good</button>
+      <button onClick={handleIncrementNeutral}>neutral</button>
+      <button onClick={handleIncrementBad}>bad</button>
+      <h1>statistics</h1>
+      <Counter counter={good} name={"good"} />
+      <Counter counter={neutral} name={"neutral"} />
+      <Counter counter={bad} name={"bad"} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,7 @@
+export const Counter = ({ counter, name }) => {
+  return (
+    <p>
+      {name} {counter}
+    </p>
+  );
+};
